6d750b8ee9b3b4a475e21b772f64f38103aa92a0
MHDF Venture Holdings <222900326+CADMP2025@users.noreply.github.com>
2025-10-09 18:03:13 +0000

    template-registry.ts update CertificateCompletion

--- lib/email/template-registry.ts
+++ lib/email/template-registry.ts
@@ -2,12 +2,14 @@ import WelcomeEmail from './templates/welcome-email';
 import PasswordResetEmail from './templates/password-reset-email';
 import EnrollmentConfirmation from './templates/enrollment-confirmation';
 import InstructorNewEnrollment from './templates/instructor-new-enrollment';
+import CertificateCompletion from './templates/certificate-completion';
 
 export const emailTemplates = {
   'welcome': WelcomeEmail,
   'password-reset': PasswordResetEmail,
   'enrollment-confirmation': EnrollmentConfirmation,
   'instructor-new-enrollment': InstructorNewEnrollment,
+  'certificate-completion': CertificateCompletion,
 } as const;
 
 export type EmailTemplate = keyof typeof emailTemplates;
