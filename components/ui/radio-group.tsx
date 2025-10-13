6bfe7c2c6c637be4873a4a28061531f9c6c8165b
MHDF Venture Holdings <222900326+CADMP2025@users.noreply.github.com>
2025-10-13 22:52:36 +0000

    fix: Add value and onValueChange support to RadioGroup

--- components/ui/radio-group.tsx
+++ components/ui/radio-group.tsx
@@ -1,23 +1,45 @@
 import * as React from "react"
 import { cn } from "@/lib/utils/cn"
 
-const RadioGroup = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => {
+interface RadioGroupContextValue {
+  value?: string
+  onValueChange?: (value: string) => void
+}
+
+const RadioGroupContext = React.createContext<RadioGroupContextValue>({})
+
+interface RadioGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  value?: string
+  onValueChange?: (value: string) => void
+}
+
+const RadioGroup = React.forwardRef<HTMLDivElement, RadioGroupProps>(
+  ({ className, value, onValueChange, ...props }, ref) => {
     return (
-      <div ref={ref} className={cn("grid gap-2", className)} {...props} />
+      <RadioGroupContext.Provider value={{ value, onValueChange }}>
+        <div ref={ref} className={cn("grid gap-2", className)} {...props} />
+      </RadioGroupContext.Provider>
     )
   }
 )
 RadioGroup.displayName = "RadioGroup"
 
-const RadioGroupItem = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
-  ({ className, ...props }, ref) => {
+interface RadioGroupItemProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string
+}
+
+const RadioGroupItem = React.forwardRef<HTMLInputElement, RadioGroupItemProps>(
+  ({ className, value: itemValue, ...props }, ref) => {
+    const { value, onValueChange } = React.useContext(RadioGroupContext)
+    
     return (
       <input
         type="radio"
         ref={ref}
+        checked={value === itemValue}
+        onChange={() => onValueChange?.(itemValue)}
         className={cn(
-          "h-4 w-4 rounded-full border border-primary text-primary focus:outline-none focus:ring-2 focus:ring-primary",
+          "h-4 w-4 rounded-full border border-primary text-primary focus:outline-none focus:ring-2 focus:ring-primary cursor-pointer",
           className
         )}
         {...props}
